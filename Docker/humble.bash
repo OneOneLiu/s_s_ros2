faea70d859a79a81a7dd505ae6d090b24bac571a
1080 Desktop <daohui.liu@mail.utoronto.ca>
2024-05-03 18:10:31 -0400

    ros2 humble

---
+++ Docker/humble.bash
@@ -0,0 +1,16 @@
+xhost local:root
+XAUTH=/tmp/.docker.xauth
+docker run --rm -it \
+    --name=ros_humble_container \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="/home/$USER/catkin_ws/src/s_s_ros2/src:/ros2_ws/src" \
+    --volume="/dev/bus/usb:/dev/bus/usb" \
+    --volume="/tmp/.docker.xauth:/tmp/.docker.xauth:rw" \
+    --net=host \
+    --privileged \
+    ros_humble_image \
+    bash
+
+echo "Done."
