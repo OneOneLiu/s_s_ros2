5e4866d111f6c283b4624b89636bf951e728e2ae
2060s-ubuntu <daohui.liu@mail.utoronto.ca>
2024-08-18 14:47:39 -0400

    update docker

---
+++ docker_rolling/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# entrypoint.sh
+
+# 移动到工作空间文件夹
+source /opt/ros/rolling/setup.bash
+cd /catkin_ws
+colcon build --symlink-install
+source /catkin_ws/install/setup.bash
+
+# 执行容器的主命令
+exec "$@"
